57498bf7bfc3fd07ef9c1c3f366a64766db1b75f
Redemoc <83233743+Redemoc@users.noreply.github.com>
2023-02-23 20:10:53 +0100

    Update the script with new parameter pattern

--- ingress.js
+++ ingress.js
@@ -1,23 +1,32 @@
+
+const TRACK_SURVEY_DURATION = true;
+
 // Redem Variables
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "http://localhost:8000";
 // https://staging.live-api.redem.io
 let SESSION_STORAGE_HELPERS = {};
-
-let global_answer;
+let redemScore = -998;
+let nextBtn = null;
+let global_answer = null;
+const QUESTION_TYPES = {
+	OES: "OES",
+	TS: "TS",
+	IBS: "IBS",
+};
 
 // Customer inputs
 const params = document.body.getElementsByTagName("script");
 query = params[0].classList;
-const questionType = query[0];
+const questionTypes = query[0]? query[0].split("|"): [];
 const questionID = query[1];
 const position = query[2];
-const hashed_magic_key = query[3];
+const datafile_secret_key = query[3];
 
 console.log(
-	"hashed_magic_key:",
-	hashed_magic_key,
-	" questionType:",
-	questionType,
+	"datafile_secret_key:",
+	datafile_secret_key,
+	" questionTypes:",
+	questionTypes,
 	" Start/End/None:",
 	position,
 	" questionID:",
@@ -34,78 +43,20 @@ function checkSurveyCompleted() {
 	return surveyCompleted;
 }
 
-function getAnswer(questionType) {
+function getAnswer() {
 	let answer;
-	if (questionType == "OES") {
+	if (questionTypes.includes(QUESTION_TYPES.OES)) {
 		const input = document.querySelector("input[id^=Q" + questionID + "A]");
-
 		if (input.type == "text") {
 			answer = input.value;
 		}
 	}
-
-	// } else if (questionType == "singleChoice") {
-	// 	const liInputs = document.getElementsByClassName("answerOrder");
-	// 	let selectedAnswerElement;
-	// 	for (const el of liInputs) {
-	// 		// For n out of m or 1 out of m selections, identify the element which has the answer by the "hidden" type
-	// 		if (el.type == "hidden") {
-	// 			selectedAnswerElement = el;
-	// 		}
-	// 	}
-	// 	let numericAnswer = Number(selectedAnswerElement.value);
-	// 	const ul = document.getElementsByTagName("ul");
-	// 	const lis = ul[0].getElementsByTagName("li");
-	// 	for (const li of lis) {
-	// 		let i = li
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByTagName("input")[0].value;
-	// 		i = Number(i[i.length - 1]);
-	// 		if (i == numericAnswer) {
-	// 			answer = li
-	// 				.getElementsByTagName("div")[0]
-	// 				.getElementsByTagName("div")[0]
-	// 				.getElementsByClassName("style-0")[0].innerHTML;
-	// 		}
-	// 	}
-	// } else if (questionType == "multipleChoice") {
-	// 	const liInputs = document.getElementsByClassName("answerOrder");
-	// 	let selectedAnswerElement;
-	// 	for (const el of liInputs) {
-	// 		// For n out of m or 1 out of m selections, identify the element which has the answer by the "hidden" type
-	// 		if (el.type == "hidden") {
-	// 			selectedAnswerElement = el;
-	// 		}
-	// 	}
-	// 	let numericAnswers = selectedAnswerElement.value
-	// 		.split(",")
-	// 		.map((v) => Number(v));
-	// 	answer = [];
-	// 	const ul = document.getElementsByTagName("ul");
-	// 	const lis = ul[0].getElementsByTagName("li");
-	// 	for (const li of lis) {
-	// 		let i = li
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByTagName("input")[0].value;
-	// 		i = Number(i[i.length - 1]);
-	// 		let textValue = li
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByTagName("div")[0]
-	// 			.getElementsByClassName("style-0")[0].innerHTML;
-	// 		if (numericAnswers.includes(i)) {
-	// 			answer.push(textValue);
-	// 		}
-	// 	}
-	// }
-
 	return answer;
 }
 
-function handleButtonClick() {
+async function handleButtonClick(startTime) {
 	// Step 1: Get answer and store in global variable
-	global_answer = getAnswer(questionType);
+	global_answer = getAnswer();
 
 	// Step 2: Get existing data from session storage
 	let payload = JSON.parse(sessionStorage.getItem("payload")) || {
@@ -114,29 +65,23 @@ function handleButtonClick() {
 		timestamps: {},
 	};
 
-	if (questionType === "OES") {
+	if (questionTypes.includes(QUESTION_TYPES.OES)) {
 		payload["open_answers"][questionID] = global_answer;
 		// } else if (questionType === "IBS") {
 		// 	// global answer is already an array of items here
 		// 	payload["item_batteries"][questionID] = global_answer;
-		// } else if (questionType === "TS") {
-		// 	let oldDeltaT = 0;
-		// 	if (Object.keys(payload["timestamps"]).includes(questionID)) {
-		// 		oldDeltaT = payload["timestamps"][questionID];
-		// 	}
-		// 	let deltaT = Date.now() - startTime;
-		// 	payload["timestamps"][questionID] = oldDeltaT + deltaT;
-	}
-	let redemScore;
-
-	// Step 3: Trigger API
-	const surveyCompleted = checkSurveyCompleted();
-	if (surveyCompleted) {
-		alert("The survey is complete. Sending payload to Redem API now ...");
-		redemScore = triggerAPI(payload);
 	}
 
-	// Step 4: Set session storage items
+	if (questionTypes.includes(QUESTION_TYPES.TS)) {
+		let oldDeltaT = 0;
+		if (Object.keys(payload["timestamps"]).includes(questionID)) {
+			oldDeltaT = payload["timestamps"][questionID];
+		}
+		let deltaT = Date.now() - startTime;
+		payload["timestamps"][questionID] = oldDeltaT + deltaT;
+	}
+
+	// Step 3: Set session storage items
 	sessionStorage.setItem(
 		"sessionStorage",
 		JSON.stringify(SESSION_STORAGE_HELPERS)
@@ -144,38 +89,62 @@ function handleButtonClick() {
 	sessionStorage.setItem("payload", JSON.stringify(payload));
 }
 
-function handleIncludeExclude(response) {
-	console.log(
-		"get api response and set to the input/screenout logic",
-		response
-	);
-}
 
-function initButtonListener() {
+async function initButtonListener() {
 	if (document.getElementById("btn_send_ahead")) {
-		var nextBtn = document.getElementById("btn_send_ahead");
-		nextBtn.onclick = handleButtonClick;
+		nextBtn = document.getElementById("btn_send_ahead");
+		const surveyCompleted = checkSurveyCompleted();
+		if (surveyCompleted) {
+			//step 1: setup the layout for API call
+			nextBtn.style.display = "none";
+			const input = document.querySelector("input[id^=Q" + questionID + "A1]");
+			input.style.display = "none";
+
+			// Step 2: Trigger API
+			redemScore = await triggerAPI(nextBtn);
+		} else {
+			//Step 1: track timestamp when question loads
+			let startTime;
+			if (questionTypes.includes(QUESTION_TYPES.TS)) {
+				startTime = Date.now();
+			}
+
+			// Step 2: track survey duration needed then add a timestamp when question loads
+			if (!SESSION_STORAGE_HELPERS["surveyStart"] && TRACK_SURVEY_DURATION) {
+				SESSION_STORAGE_HELPERS["surveyStart"] = Date.now();
+				sessionStorage.setItem("sessionStorage", JSON.stringify(SESSION_STORAGE_HELPERS));
+			}
+
+			// Step 3: add listener
+			nextBtn.addEventListener("click", async () => {
+				await handleButtonClick(startTime);
+			});
+		}
 	} else {
 		window.setTimeout("initButtonListener()", 10);
 	}
 }
 
 function setRedemScoreToAnswer(redemScore) {
-	let inputs = document.getElementsByTagName("input");
-	for (const el of inputs) {
-		if (el.type == "number") {
-			el.value = redemScore;
-			el.click();
-		}
-	}
+	const input = document.querySelector("input[id^=Q" + questionID + "A1]");
+	input.value = redemScore;
+	input.click();
+
+	// click the button and proceed
+	nextBtn.style.display = "";
+	nextBtn.click();
 }
 
-async function triggerAPI(payload) {
+async function triggerAPI(nextBtn) {
+	// Step 1: get payload from storage
+	let payload = JSON.parse(sessionStorage.getItem("payload"));
+
+	// Step 2: format data
 	const datapoints = [];
 	for (let i = 0; i < Object.keys(payload["open_answers"]).length; i++) {
 		let questionID = Object.keys(payload["open_answers"])[i];
 		datapoints.push({
-			dataPointIdentifier: questionID,
+			dataPointIdentifier: "Q" + (i + 1) + "_OES",
 			openEndedAnswer: payload["open_answers"][questionID],
 		});
 	}
@@ -187,61 +156,59 @@ async function triggerAPI(payload) {
 	// 		numberOfItems: payload["item_batteries"][questionID].length,
 	// 	});
 	// }
-	// for (let i = 0; i < Object.keys(payload["timestamps"]).length; i++) {
-	// 	let questionID = Object.keys(payload["timestamps"])[i];
-	// 	datapoints.push({
-	// 		dataPointIdentifier: questionID,
-	// 		timeStamp: payload["timestamps"][questionID],
-	// 	});
 
-	let redemScore = -998;
+	for (let i = 0; i < Object.keys(payload["timestamps"]).length; i++) {
+		let questionID = Object.keys(payload["timestamps"])[i];
+		datapoints.push({
+			dataPointIdentifier: "Q" + (i + 1)+ "_TS",
+			timeStamp: payload["timestamps"][questionID],
+		});
+	}
 
-	fetch(`${BASE_URL}/live-respondent/create`, {
-		method: "POST",
-		mode: "cors",
-		body: JSON.stringify({
-			respondentID: `${new Date().getTime()}`,
-			datapoints: datapoints,
-			hashed_magic_key: hashed_magic_key,
-		}),
-		referrer: "origin",
-		headers: {
-			"Content-Type": "application/json",
-		},
-	})
-		.then(function (response) {
-			// The API call was successful!
-			if (response.ok) {
-				return response.json();
-			} else {
-				return Promise.reject(response);
-			}
-		})
-		.then(function (data) {
-			// This is the JSON from our response
-			console.warn("Successfull!", data);
-			handleIncludeExclude(data);
-			alert("Success:" + JSON.stringify(data));
-		})
-		.catch(function (err) {
-			// There was an error
-			console.warn("Something went wrong ->", err);
-			alert("error:" + err.message);
+	// Step 3: If total duration is enabled add it as a seperate datapoint
+	if(TRACK_SURVEY_DURATION){
+		SESSION_STORAGE_HELPERS["surveyCompleted"] = Date.now();
+		const totalSurveyDuration = SESSION_STORAGE_HELPERS["surveyStart"] ? SESSION_STORAGE_HELPERS["surveyCompleted"] - SESSION_STORAGE_HELPERS["surveyStart"] : undefined;
+		// alert("The survey is complete. Total duration: "+String(totalSurveyDuration));
+		datapoints.push({
+			dataPointIdentifier: "Total_TS",
+			timeStamp: totalSurveyDuration,
+		});
+	}
+
+	// Step 4: Call the API
+	try {
+		const res = await fetch(`${BASE_URL}/live-respondent/create`, {
+			method: "POST",
+			mode: "cors",
+			body: JSON.stringify({
+				respondentID: `${new Date().getTime()}`,
+				datapoints: datapoints,
+				datafile_secret_key: datafile_secret_key,
+			}),
+			referrer: "origin",
+			headers: {
+				"Content-Type": "application/json",
+			},
 		});
 
-	// 	alert(data.message);
-	// 	const data = await res.json();
-	// 	redemScore = data.body.redemScore;
-	// 	alert("API call succeeded!");
-	// 	alert(data.message);
-	// } catch (error) {
-	// 	alert("API call failed");
-	// 	redemScore = -997;
-	// 	alert(error);
-	// } finally {
-	// 	alert("Redem Score in finally block: " + String(redemScore));
-	// 	return redemScore;
-	// }
+		const data = await res.json();
+		redemScore = (data.body.redemScore && data.body.redemScore >=0)?  data.body.redemScore : -997;
+	} catch (error) {
+		alert("API call failed");
+		// alert(error);
+		redemScore = -997;
+	} finally {
+		// alert("Redem Score in finally block: " + String(redemScore));
+		//always proceed with the redem score not affecting the user flow of the survey tool
+		setRedemScoreToAnswer(redemScore);
+		clearSessionStorage();
+		return redemScore;
+	}
+}
+
+function clearSessionStorage(){
+	console.log("Clear Here");
 }
 
 SESSION_STORAGE_HELPERS = JSON.parse(
@@ -249,6 +216,7 @@ SESSION_STORAGE_HELPERS = JSON.parse(
 ) || {
 	totalScore: -999,
 	surveyStart: false,
+	surveyCompleted: false
 };
 
 initButtonListener();
