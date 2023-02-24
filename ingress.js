3b8b3767938911953fe35a7ebb549dcc3c97013f
Redemoc <83233743+Redemoc@users.noreply.github.com>
2023-02-24 16:37:23 +0100

    Add support for IBS in payload

--- ingress.js
+++ ingress.js
@@ -1,26 +1,26 @@
-
+// info "|"" and "[0]" doesnt work on ingress inside of a script inside table
 const TRACK_SURVEY_DURATION = true;
 
 // Redem Variables
 const BASE_URL = "http://localhost:8000";
-// https://staging.live-api.redem.io
 let SESSION_STORAGE_HELPERS = {};
 let redemScore = -998;
 let nextBtn = null;
 let global_answer = null;
-const QUESTION_TYPES = {
+const SCORE_TYPES = {
 	OES: "OES",
 	TS: "TS",
 	IBS: "IBS",
 };
 
 // Customer inputs
-const params = document.body.getElementsByTagName("script");
-query = params[0].classList;
-const questionTypes = query[0]? query[0].split("|"): [];
-const questionID = query[1];
-const position = query[2];
-const datafile_secret_key = query[3];
+var script = document.currentScript;
+var query = script.getAttribute('data-params').split(" ")
+
+const questionTypes = query.at(0) ? query.at(0).split("+") : new Array();
+const questionID = query.at(1);
+const position =  query.at(2);
+const datafile_secret_key =  query.at(3);
 
 console.log(
 	"datafile_secret_key:",
@@ -45,10 +45,18 @@ function checkSurveyCompleted() {
 
 function getAnswer() {
 	let answer;
-	if (questionTypes.includes(QUESTION_TYPES.OES)) {
+	if (questionTypes.includes(SCORE_TYPES.OES)) {
 		const input = document.querySelector("input[id^=Q" + questionID + "A]");
 		if (input.type == "text") {
 			answer = input.value;
+		}
+	} else if (questionTypes.includes(SCORE_TYPES.IBS)) {
+		answer = [];
+		const inputs = document.querySelectorAll("input[id^=Q" + questionID + "A]");
+		for (const input of inputs) {
+			if (input.type == "radio" && input.checked) {
+				answer.push(input.value);
+			}
 		}
 	}
 	return answer;
@@ -59,20 +67,19 @@ async function handleButtonClick(startTime) {
 	global_answer = getAnswer();
 
 	// Step 2: Get existing data from session storage
-	let payload = JSON.parse(sessionStorage.getItem("payload")) || {
+	let payload = sessionStorage.getItem("payload") ? JSON.parse(sessionStorage.getItem("payload")) : {
 		open_answers: {},
 		item_batteries: {},
 		timestamps: {},
 	};
 
-	if (questionTypes.includes(QUESTION_TYPES.OES)) {
+	if (questionTypes.includes(SCORE_TYPES.OES)) {
 		payload["open_answers"][questionID] = global_answer;
-		// } else if (questionType === "IBS") {
-		// 	// global answer is already an array of items here
-		// 	payload["item_batteries"][questionID] = global_answer;
+	} else if (questionTypes.includes(SCORE_TYPES.IBS)) {
+		payload["item_batteries"][questionID] = global_answer;
 	}
 
-	if (questionTypes.includes(QUESTION_TYPES.TS)) {
+	if (questionTypes.includes(SCORE_TYPES.TS)) {
 		let oldDeltaT = 0;
 		if (Object.keys(payload["timestamps"]).includes(questionID)) {
 			oldDeltaT = payload["timestamps"][questionID];
@@ -89,7 +96,6 @@ async function handleButtonClick(startTime) {
 	sessionStorage.setItem("payload", JSON.stringify(payload));
 }
 
-
 async function initButtonListener() {
 	if (document.getElementById("btn_send_ahead")) {
 		nextBtn = document.getElementById("btn_send_ahead");
@@ -105,14 +111,17 @@ async function initButtonListener() {
 		} else {
 			//Step 1: track timestamp when question loads
 			let startTime;
-			if (questionTypes.includes(QUESTION_TYPES.TS)) {
+			if (questionTypes.includes(SCORE_TYPES.TS)) {
 				startTime = Date.now();
 			}
 
 			// Step 2: track survey duration needed then add a timestamp when question loads
 			if (!SESSION_STORAGE_HELPERS["surveyStart"] && TRACK_SURVEY_DURATION) {
 				SESSION_STORAGE_HELPERS["surveyStart"] = Date.now();
-				sessionStorage.setItem("sessionStorage", JSON.stringify(SESSION_STORAGE_HELPERS));
+				sessionStorage.setItem(
+					"sessionStorage",
+					JSON.stringify(SESSION_STORAGE_HELPERS)
+				);
 			}
 
 			// Step 3: add listener
@@ -148,27 +157,31 @@ async function triggerAPI(nextBtn) {
 			openEndedAnswer: payload["open_answers"][questionID],
 		});
 	}
-	// for (let i = 0; i < Object.keys(payload["item_batteries"]).length; i++) {
-	// 	let questionID = Object.keys(payload["item_batteries"])[i];
-	// 	datapoints.push({
-	// 		dataPointIdentifier: questionID,
-	// 		itemBattery: payload["item_batteries"][questionID],
-	// 		numberOfItems: payload["item_batteries"][questionID].length,
-	// 	});
-	// }
+
+	for (let i = 0; i < Object.keys(payload["item_batteries"]).length; i++) {
+		let questionID = Object.keys(payload["item_batteries"])[i];
+		datapoints.push({
+			dataPointIdentifier: "Q" + (i + 1) + "_IBS",
+			itemBattery: payload["item_batteries"][questionID],
+			numberOfItems: payload["item_batteries"][questionID].length,
+		});
+	}
 
 	for (let i = 0; i < Object.keys(payload["timestamps"]).length; i++) {
 		let questionID = Object.keys(payload["timestamps"])[i];
 		datapoints.push({
-			dataPointIdentifier: "Q" + (i + 1)+ "_TS",
+			dataPointIdentifier: "Q" + (i + 1) + "_TS",
 			timeStamp: payload["timestamps"][questionID],
 		});
 	}
 
 	// Step 3: If total duration is enabled add it as a seperate datapoint
-	if(TRACK_SURVEY_DURATION){
+	if (TRACK_SURVEY_DURATION) {
 		SESSION_STORAGE_HELPERS["surveyCompleted"] = Date.now();
-		const totalSurveyDuration = SESSION_STORAGE_HELPERS["surveyStart"] ? SESSION_STORAGE_HELPERS["surveyCompleted"] - SESSION_STORAGE_HELPERS["surveyStart"] : undefined;
+		const totalSurveyDuration = SESSION_STORAGE_HELPERS["surveyStart"]
+			? SESSION_STORAGE_HELPERS["surveyCompleted"] -
+			  SESSION_STORAGE_HELPERS["surveyStart"]
+			: undefined;
 		// alert("The survey is complete. Total duration: "+String(totalSurveyDuration));
 		datapoints.push({
 			dataPointIdentifier: "Total_TS",
@@ -193,7 +206,10 @@ async function triggerAPI(nextBtn) {
 		});
 
 		const data = await res.json();
-		redemScore = (data.body.redemScore && data.body.redemScore >=0)?  data.body.redemScore : -997;
+		redemScore =
+			data.body.redemScore && data.body.redemScore >= 0
+				? data.body.redemScore
+				: -997;
 	} catch (error) {
 		alert("API call failed");
 		// alert(error);
@@ -207,16 +223,16 @@ async function triggerAPI(nextBtn) {
 	}
 }
 
-function clearSessionStorage(){
+function clearSessionStorage() {
 	console.log("Clear Here");
 }
 
-SESSION_STORAGE_HELPERS = JSON.parse(
+SESSION_STORAGE_HELPERS = sessionStorage.getItem("sessionStorage") ? JSON.parse(
 	sessionStorage.getItem("sessionStorage")
-) || {
+) : {
 	totalScore: -999,
 	surveyStart: false,
-	surveyCompleted: false
+	surveyCompleted: false,
 };
 
 initButtonListener();
